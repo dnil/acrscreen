4f924fbfb1606503afafdef623c5a74df7258c59
Daniel Nilsson <daniel.nilsson@ki.se>
2011-03-08 14:28:47 +0100

    Multiple mira assemblies to get per-worm genotypes.

--- bin/run_screen.sh
+++ bin/run_screen.sh
@@ -32,6 +32,7 @@ POLYPHREDPHD2IUPACFASTA=/home/daniel/sandbox/acrscreen/bin/polyphredphd2iupacfas
 
 CHECKPOLY=/home/daniel/sandbox/acrscreen/bin/checkpoly.pl
 PIPELINEFUNK=/home/daniel/sandbox/acrscreen/bin/pipelinefunk.sh
+FASTAHEADERGREP=~/sandbox/Cestunepipe/bin/fasta_header_grep.pl
 
 REFERENCE=/home/daniel/sandbox/acrscreen/eiire.fasta
 REFSTRING=.c1
@@ -291,7 +292,7 @@ then
 #	registerFile ${PWD}/$isolate.isolate.iupac.fasta temp
     done
 
-    registerFile $PWD/${BASE}.all.iupac.fasta result
+    registerFile $PWD/${iupacfasta} result
 
  #   for isolatefasta in *isolate.iupac.fasta
  #   do
@@ -300,15 +301,24 @@ then
 fi
 
 # per worm assembly, retaining iupac genotypes - mira
-#
-# for each worm, 
-#  cat ../edit_dir/NZL0012-59*ab1*polyphred.iupac.fasta > NZL0012-59_in.fasta
-#  ~/sandbox/Cestunepipe/bin/fasta_header_grep.pl NZL0012-59 ../edit_dir/hc_mptl_screen_20110216.ref.fasta.consedified.qual > NZL0012-59_in.sanger.fasta.qual
-#  ln -s ../edit_dir/EII.c1.polyphred.iupac.fasta NZL0012-59_backbone_in.fasta
-#  mira --project=NZL0012-59 --job=mapping,sanger -SB:bbq=20 --fasta >NZL0012-59.mira.log
-#  cat NZL0012-59_assembly/NZL0012-59_d_results/NZL0012-59_assembly/NZL0012-59_d_results/NZL0012-59_out.unpadded.fasta >> ${BASE}.wormwise.all.iupac.fasta
-#
-#
+
+wormwiseiupacfasta=${BASE}.wormwise.all.iupac.fasta
+
+
+if needsUpdate $wormwiseiupacfasta $iupacfasta $FASTA_HEADER_GREP ${BASE}.ref.fasta.consedified.qual $REFERENCE
+then
+    # or ls -1 chromat_dir instead of $wormfarmname
+    for worm in `cut -f 1,2 -d- $wormfarmname |sort |uniq`
+    do
+	cat $worm*ab1*polyphred.iupac.fasta > ${worm}_in.fasta
+	$FASTAHEADERGREP $worm ${BASE}.ref.fasta.consedified.qual > ${worm}_in.sanger.fasta.qual
+	ln -s $REFERENCE ${worm}_backbone_in.fasta
+	mira --project=${worm} --job=mapping,sanger -SB:bbq=20 --fasta >${worm}.mira.log
+	rm ${worm}_backbone_in.fasta
+	cat ${worm}_assembly/${worm}_d_results/${worm}_assembly/${worm}_d_results/${worm}_out.unpadded.fasta >> ${BASE}.wormwise.all.iupac.fasta
+	# rm -rf ${worm}_assembly
+    done
+fi
 
 # log stats
 
